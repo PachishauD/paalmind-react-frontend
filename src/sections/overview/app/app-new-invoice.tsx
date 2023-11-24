409469c4d7053e0071922cec5854bd4aa6f12052
daniel <danjchang0418@gmail.com>
2023-11-25 04:14:53 +0900

    first Commit

---
+++ src/sections/overview/app/app-new-invoice.tsx
@@ -0,0 +1,161 @@
+// @mui
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import MenuItem from '@mui/material/MenuItem';
+import TableRow from '@mui/material/TableRow';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import CardHeader from '@mui/material/CardHeader';
+import IconButton from '@mui/material/IconButton';
+import Card, { CardProps } from '@mui/material/Card';
+import TableContainer from '@mui/material/TableContainer';
+// utils
+import { fCurrency } from 'src/utils/format-number';
+// components
+import Label from 'src/components/label';
+import Iconify from 'src/components/iconify';
+import Scrollbar from 'src/components/scrollbar';
+import CustomPopover, { usePopover } from 'src/components/custom-popover';
+import { TableHeadCustom } from 'src/components/table';
+
+// ----------------------------------------------------------------------
+
+type RowProps = {
+  username: string,
+  wallet_address: string,
+  hash: string,
+  type: string,
+  amount: number,
+  token: string,
+  timestamp: string,
+};
+
+interface Props extends CardProps {
+  title?: string;
+  subheader?: string;
+  tableData: Array<any>;
+  tableLabels: Array<any>;
+}
+
+export default function AppNewInvoice({
+  title,
+  subheader,
+  tableData,
+  tableLabels,
+  ...other
+}: Props) {
+  console.log(tableData);
+  return (
+    <Card {...other}>
+      <CardHeader title={title} subheader={subheader} sx={{ mb: 3 }} />
+
+      <TableContainer sx={{ overflow: 'unset' }}>
+        <Scrollbar>
+          <Table sx={{ minWidth: 680 }}>
+            <TableHeadCustom headLabel={tableLabels as any[]} />
+
+            <TableBody>
+              {tableData.map((row: any) => (
+                <AppNewInvoiceRow row={row} />
+              ))}
+            </TableBody>
+          </Table>
+        </Scrollbar>
+      </TableContainer>
+
+      <Divider sx={{ borderStyle: 'dashed' }} />
+
+      <Box sx={{ p: 2, textAlign: 'right' }}>
+        <Button
+          size="small"
+          color="inherit"
+          endIcon={<Iconify icon="eva:arrow-ios-forward-fill" width={18} sx={{ ml: -0.5 }} />}
+        >
+          View All
+        </Button>
+      </Box>
+    </Card>
+  );
+}
+
+
+
+// ----------------------------------------------------------------------
+
+// type AppNewInvoiceRowProps = {
+//   row: RowProps;
+// };
+
+interface NewProps {
+  row: Array<any>
+}
+
+function AppNewInvoiceRow(props: NewProps) {
+  const { row } = props;
+  const popover = usePopover();
+
+  const handleDownload = () => {
+    popover.onClose();
+    console.info('DOWNLOAD', row[0]);
+  };
+
+  const handlePrint = () => {
+    popover.onClose();
+    console.info('PRINT', row[0]);
+  };
+
+  const handleShare = () => {
+    popover.onClose();
+    console.info('SHARE', row[0]);
+  };
+
+  const handleDelete = () => {
+    popover.onClose();
+    console.info('DELETE', row[0]);
+  };
+
+  return (
+    <>
+      <TableRow>
+        <TableCell>{row[0]}</TableCell>
+        <TableCell>{row[1]}</TableCell>
+        <TableCell>{row[2]}</TableCell>
+        <TableCell>{row[3]}</TableCell>
+        <TableCell>{row[4]}</TableCell>
+        <TableCell>{row[5]}</TableCell>
+        <TableCell>{row[6]}</TableCell>
+      </TableRow>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="right-top"
+        sx={{ width: 160 }}
+      >
+        <MenuItem onClick={handleDownload}>
+          <Iconify icon="eva:cloud-download-fill" />
+          Download
+        </MenuItem>
+
+        <MenuItem onClick={handlePrint}>
+          <Iconify icon="solar:printer-minimalistic-bold" />
+          Print
+        </MenuItem>
+
+        <MenuItem onClick={handleShare}>
+          <Iconify icon="solar:share-bold" />
+          Share
+        </MenuItem>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <MenuItem onClick={handleDelete} sx={{ color: 'error.main' }}>
+          <Iconify icon="solar:trash-bin-trash-bold" />
+          Delete
+        </MenuItem>
+      </CustomPopover>
+    </>
+  );
+}
---
+++ src/sections/overview/app/view/overview-app-view.tsx
@@ -0,0 +1,248 @@
+// @mui
+import React, { useState, useEffect } from 'react';
+import { useTheme } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+// hooks
+import { useMockedUser } from 'src/hooks/use-mocked-user';
+// _mock
+import { _appFeatured, _appAuthors, _appInstalled, _appRelated, _appInvoices } from 'src/_mock';
+// components
+import { useSettingsContext } from 'src/components/settings';
+// assets
+import { SeoIllustration } from 'src/assets/illustrations';
+//
+import axios from 'axios';
+import AppWidget from '../app-widget';
+import AppWelcome from '../app-welcome';
+import AppFeatured from '../app-featured';
+import AppNewInvoice from '../app-new-invoice';
+import AppTopAuthors from '../app-top-authors';
+import AppTopRelated from '../app-top-related';
+import AppAreaInstalled from '../app-area-installed';
+import AppWidgetSummary from '../app-widget-summary';
+import AppCurrentDownload from '../app-current-download';
+import AppTopInstalledCountries from '../app-top-installed-countries';
+
+// ----------------------------------------------------------------------
+
+export default function OverviewAppView() {
+  // const { user } = useMockedUser();
+  const user = {
+    name: "Steven",
+    email: "paalmind@gmail.com",
+    displayName: "steven(Admin)"
+  }
+  const [users, setUsers] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+
+  const theme = useTheme();
+
+  const settings = useSettingsContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https:///b630-34-135-72-108.ngrok-free.app/customers', {
+          headers: {
+            'ngrok-skip-browser-warning': '69420',
+          },
+        });
+        setUsers(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      try {
+        const response = await axios.get('https:///b630-34-135-72-108.ngrok-free.app/transactions', {
+          headers: {
+            'ngrok-skip-browser-warning': '69420',
+          },
+        });
+        setTransactions(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const numUsers = users.length;
+  const numTransactions = transactions.length;
+
+  return (
+    <Container maxWidth={settings.themeStretch ? false : 'xl'}>
+      <Grid container spacing={3}>
+        <Grid xs={12} md={8}>
+          <AppWelcome
+            title={`Welcome back 👋 \n ${user?.displayName[0].toUpperCase()}${user?.displayName.slice(1)}`}
+            description="If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything."
+            img={<SeoIllustration />}
+            action={
+              <Button variant="contained" color="primary">
+                Go Now
+              </Button>
+            }
+          />
+        </Grid>
+
+        <Grid xs={12} md={4}>
+          <AppFeatured list={_appFeatured} />
+        </Grid>
+
+        <Grid xs={12} md={4}>
+          <AppWidgetSummary
+            title="Total Active Users"
+            percent={0.1}
+            total={numUsers}
+            chart={{
+              series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={4}>
+          <AppWidgetSummary
+            title="Total Transactions"
+            percent={0.2}
+            total={numTransactions}
+            chart={{
+              colors: [theme.palette.info.light, theme.palette.info.main],
+              series: [20, 41, 63, 33, 28, 35, 50, 46, 11, 26],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={4}>
+          <AppWidgetSummary
+            title="Total Profits"
+            percent={0.0}
+            total={0}
+            chart={{
+              colors: [theme.palette.warning.light, theme.palette.warning.main],
+              series: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            }}
+            unit="$"
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppCurrentDownload
+            title="Bets Stats"
+            chart={{
+              series: [
+                { label: 'Won', value: 10 },
+                { label: 'Loss', value: 5 },
+                { label: 'Pending', value: 6 },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={8}>
+          <AppAreaInstalled
+            title="Sport Betting vs Casino Live Status"
+            subheader="(+43%) than last year"
+            chart={{
+              categories: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec',
+              ],
+              series: [
+                {
+                  year: '2022',
+                  data: [
+                    {
+                      name: 'Sport Betting',
+                      data: [10, 41, 35, 51, 49, 62, 69, 91, 148, 35, 51, 49],
+                    },
+                    {
+                      name: 'Casino',
+                      data: [10, 34, 13, 56, 77, 88, 99, 77, 45, 13, 56, 77],
+                    },
+                  ],
+                },
+                {
+                  year: '2023',
+                  data: [
+                    {
+                      name: 'Sport Betting',
+                      data: [51, 35, 41, 10, 91, 69, 62, 148, 91, 69, 62, 49],
+                    },
+                    {
+                      name: 'Casino',
+                      data: [56, 13, 34, 10, 77, 99, 88, 45, 77, 99, 88, 77],
+                    },
+                  ],
+                },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} lg={12}>
+          <AppNewInvoice
+            title="Transactions History"
+            tableData={transactions}
+            tableLabels={[
+              { id: 'username', label: 'Username' },
+              { id: 'wallet_address', label: 'Wallet Address' },
+              { id: 'hash', label: 'hash' },
+              { id: 'type', label: 'type' },
+              { id: 'amount', label: 'amount' },
+              { id: 'token', label: 'token' },
+              { id: 'timestamp', label: 'timestamp' },
+              { id: '' },
+            ]}
+          />
+        </Grid>
+
+        {/* <Grid xs={12} md={6} lg={4}>
+          <AppTopRelated title="Top Related Applications" list={_appRelated} />
+        </Grid> */}
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppTopInstalledCountries title="Top Installed Countries" list={_appInstalled} />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppTopAuthors title="Top Authors" list={_appAuthors} />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <Stack spacing={3}>
+            <AppWidget
+              title="Conversion"
+              total={38566}
+              icon="solar:user-rounded-bold"
+              chart={{
+                series: 48,
+              }}
+            />
+
+            <AppWidget
+              title="Applications"
+              total={55566}
+              icon="fluent:mail-24-filled"
+              color="info"
+              chart={{
+                series: 75,
+              }}
+            />
+          </Stack>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
